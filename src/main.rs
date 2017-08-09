9cd07e6c95791c5319778866aa9ab8d0b256cdd7
Jacob Killelea <Jkillelea@protonmail.ch>
2017-08-09 12:41:57 -0700

    load and parse all requests sequentially

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ extern crate reqwest;
 extern crate url;
 use url::{Url, ParseError};
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
 extern crate serde_json;
 use serde_json::Value;
 use Value::{Array, Number};
@@ -19,6 +22,30 @@ use std::sync::mpsc;
 
 const NROWS:    u64  = 1000;
 const BASE_URL: &str = "https://hacker-news.firebaseio.com/";
+
+// {
+//   "by" : "artsandsci",
+//   "descendants" : 16,
+//   "id" : 14970171,
+//   "kids" : [ 14971679, 14971331, 14971597, 14970967, 14970908, 14971481, 14970936, 14970742 ],
+//   "score" : 48,
+//   "time" : 1502292702,
+//   "title" : "Bitcoin Makes Even Smart People Feel Dumb",
+//   "type" : "story",
+//   "url" : "https://www.wired.com/story/bitcoin-makes-even-smart-people-feel-dumb"
+// }
+#[derive(Serialize, Deserialize, Debug)]
+struct HnArticle {
+    id:           usize,
+    by:           String,
+    url:          Option<String>,
+    score:        usize,
+    time:         usize,
+    title:        String,
+    descendants:  Option<usize>,
+    kids:         Option<Vec<usize>>,
+}
+
 
 fn main() {
     let mut stdout = io::stdout();
@@ -44,25 +71,25 @@ fn main() {
             let mut res = client.get(url).unwrap().send().unwrap();
             res.read_to_string(&mut response_body);
 
-            // println!("{:?}", response_body);
-            stdout.write(&response_body.as_bytes()).unwrap();
+            match serde_json::from_str::<HnArticle>(&response_body) {
+                Ok(json) => {
+                    // println!("{:#?}", json);
+                    println!("{}: {}", i, json.title);
+                    if json.url.is_some() {
+                        println!("\t{}", json.url.unwrap());
+                    }
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
+            }
+
             i += 1;
         }
         println!("Retrieved {} out of {} posts", i, n_posts);
     }
 
     // gtk_stuff();
-}
-
-fn make_request(client: &reqwest::Client, url: Url) -> reqwest::RequestBuilder {
-    let url_string = url.to_string();
-    match client.get(url) {
-        Ok(request) => return request,
-        Err(_) => {
-            thread::sleep(::std::time::Duration::new(1, 0));
-            return make_request(client, Url::parse(&url_string).unwrap());
-        }
-    }
 }
 
 fn topstories_url() -> Result<Url, ParseError> {
